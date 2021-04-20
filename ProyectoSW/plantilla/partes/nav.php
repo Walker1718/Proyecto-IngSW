60a16a75ffe05627df129c90049058b9051bbfc7
Walker1718 <carlos.aip212@gmail.com>
2021-04-19 22:37:38 -0400

    pruebas con plantilla

--- ProyectoSW/plantilla/partes/nav.php
+++ ProyectoSW/plantilla/partes/nav.php
@@ -16,10 +16,10 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="https://previews.123rf.com/images/jemastock/jemastock1609/jemastock160905175/63043795-persona-ejecutiva-en-traje-con-la-ilustraci%C3%B3n-de-vector-de-imagen-de-iconos-de-negocios-relacionados-co.jpg" 
+                        <!-- <img src="https://previews.123rf.com/images/jemastock/jemastock1609/jemastock160905175/63043795-persona-ejecutiva-en-traje-con-la-ilustraci%C3%B3n-de-vector-de-imagen-de-iconos-de-negocios-relacionados-co.jpg" 
                         class="img-fluid rounded-circle avatar mr-2"
-                        alt="imagen" />
-                        Juan Íto
+                        alt="imagen" /> -->
+                        AAAAAAAAAAAAAAAAAAA
                         </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="../perfil/"><i class="fas fa-user"></i> Mi perfil</a>
--- ProyectoSW/plantilla/partes/sidebar.php
+++ ProyectoSW/plantilla/partes/sidebar.php
@@ -1,8 +1,13 @@
 <!-- Sidebar -->
-<div id="sidebar-container" class="bg-primary">
+<div id="sidebar-container" class="bg-primary  ">
             <div class="logo">
-                <h4 class="text-light font-weight-bold mb-0">INGENIERÍA DE SOFTWARE</h4>
+                <img src="../resources/images/logo" alt="logo" width="50" height="80">
+
+    
+
+                <h4 class="text-light font-weight-bold mb-0">Aperture aaaa</h4>
             </div>
+
             <div class="menu">
                 <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
                     Inicio</a>
@@ -36,7 +41,7 @@
                 <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-chart-line"></i>
                     Estadísticas</a>
 
-                <a href="../perfil/" class="d-block text-light p-3 border-0"><i class="fas fa-user"></i>
+                <a href="../perfil/" class="d-block text-light p-3 border-0"><i class="fas fa-user-circle"></i>
                     Perfil</a>
 
                 <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-sliders-h"></i>
@@ -44,3 +49,59 @@
             </div>
         </div>
         <!-- Fin sidebar -->
+
+
+
+        <!-- <div class="app-body">
+    <div class="sidebar">
+      <nav class="sidebar-nav">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="main.html"><i class="icon-speedometer"></i> Dashboard <span class="badge badge-primary">NEW</span></a>
+          </li>
+
+          <li class="nav-title">
+            UI Elements
+          </li>
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Components</a>
+            <ul class="nav-dropdown-items">
+              <li class="nav-item">
+                <a class="nav-link" href="components/buttons.html"><i class="icon-puzzle"></i> Buttons</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="components/social-buttons.html"><i class="icon-puzzle"></i> Social Buttons</a>
+              </li>
+
+            </ul>
+          </li>
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-star"></i> Icons</a>
+            <ul class="nav-dropdown-items">
+              <li class="nav-item">
+                <a class="nav-link" href="icons/font-awesome.html"><i class="icon-star"></i> Font Awesome <span class="badge badge-secondary">4.7</span></a>
+              </li>
+
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="widgets.html"><i class="icon-calculator"></i> Widgets <span class="badge badge-primary">NEW</span></a>
+          </li>
+
+            Extras
+          </li>
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-star"></i> Pages</a>
+            <ul class="nav-dropdown-items">
+              <li class="nav-item">
+                <a class="nav-link" href="views/pages/login.html" target="_top"><i class="icon-star"></i> Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="views/pages/register.html" target="_top"><i class="icon-star"></i> Register</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+    </div> -->
